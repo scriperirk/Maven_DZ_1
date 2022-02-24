b5d9886bdd318b103b7134bbe41886700a38e249
Scriper.irk <scriper.irk@gmail.com>
2022-02-24 21:58:13 +0800

    initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,22 @@
 public class Main {
+    public static void main(String[] args) {
+
+        BonusService service = new BonusService();
+
+        //подготовка данных
+        long amount = 1000_68;
+        boolean registered = true;
+        long expected = 30;
+
+        //вызов целевого метода
+        long actual = service.calculate(amount,registered);
+
+        boolean passed = expected == actual;
+
+        //вывод результата
+        System.out.println(passed);
+    }
+
+
+
 }
